f01cbe0defabb1364a2cac310818df0d18928cec
robinsonianr <robinsonir02@gmail.com>
2024-07-25 00:21:35 -0400

    implemented book submission

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -5,6 +5,7 @@ import (
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 	"github.com/ianrr/library/internal/api"
 	"github.com/ianrr/library/internal/db"
 )
@@ -21,20 +22,22 @@ func initServer() {
 
     
     r := chi.NewRouter()
+    r.Use(middleware.Logger)
 
     r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
         w.Write([]byte("Hello, World!"))
     })
+
+    // methods of bookhandler as HTTP handlers
     r.Get("/books", bookHandler.GetBooks)
+    r.Get("/book/{id}", bookHandler.GetBook)
+    r.Post("/book", bookHandler.SubmitBook)
     
     http.ListenAndServe(":8080", r)
-
 }
 
 
 func main() {
-    //server.MountHandler()
-    //server := app.CreateNewServer()
     log.Println("Hello, World!")
     initServer()
 }
